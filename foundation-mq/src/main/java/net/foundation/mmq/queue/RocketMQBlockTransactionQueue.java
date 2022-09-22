f98be9caae8fc1beed6b894bdb551303627eea60
chiangfire <chiangfire@localhost.localdomain>
2022-09-22 21:04:01 +0800

    fix

--- foundation-mq/src/main/java/net/foundation/mmq/queue/RocketMQBlockTransactionQueue.java
+++ foundation-mq/src/main/java/net/foundation/mmq/queue/RocketMQBlockTransactionQueue.java
@@ -6,7 +6,6 @@ import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
