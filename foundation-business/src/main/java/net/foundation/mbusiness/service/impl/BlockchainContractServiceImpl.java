a0856e79ae61c0a44280b215b0425f31a70cc2f0
chiangfire <chiangfire@localhost.localdomain>
2022-09-22 09:55:57 +0800

    fix

--- foundation-business/src/main/java/net/foundation/mbusiness/service/impl/BlockchainContractServiceImpl.java
+++ foundation-business/src/main/java/net/foundation/mbusiness/service/impl/BlockchainContractServiceImpl.java
@@ -2,8 +2,10 @@ package net.foundation.mbusiness.service.impl;
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import lombok.extern.slf4j.Slf4j;
 import net.foundation.mbusiness.domain.BlockchainContract;
 import net.foundation.mbusiness.domain.BlockchainContractInfo;
 import net.foundation.mbusiness.mapper.BlockchainContractMapper;
@@ -16,36 +18,58 @@ import org.springframework.stereotype.Service;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class BlockchainContractServiceImpl extends ServiceImpl<BlockchainContractMapper, BlockchainContract> implements BlockchainContractService {
 
-    private Cache<String, BlockchainContractInfo> cache = CacheBuilder.newBuilder()
-                                                                      .concurrencyLevel(50)
-                                                                      .expireAfterWrite(3, TimeUnit.HOURS)
-                                                                      .initialCapacity(200)
-                                                                      .maximumSize(1000)
-                                                                      .build();
+    private final LoadingCache<String, BlockchainContractInfo> cache;
+
+    public BlockchainContractServiceImpl() {
+        BlockchainContractService service = this;
+        CacheLoader<String, BlockchainContractInfo> cacheLoader = new CacheLoader<>() {
+            @Override
+            public BlockchainContractInfo load(String contractAddr) throws Exception {
+                System.err.println("正在加载缓存!");
+                var query = Wrappers.lambdaQuery(BlockchainContract.class).eq(BlockchainContract::getAddr,contractAddr);
+                BlockchainContract bc = service.getOne(query);
+                BlockchainContractInfo info = new BlockchainContractInfo();
+                if(Objects.nonNull(bc)) {
+                    BeanUtils.copyProperties(bc,info);
+                    if(StringUtils.isNoneBlank(bc.getAbi())) {
+                        info.setAbiDecoder(createAbiDecoder(bc.getAbi()));
+                    }
+                }
+                return info;
+            }
+        };
+        this.cache = CacheBuilder.newBuilder()
+                                 .concurrencyLevel(50)
+                                 .expireAfterWrite(3, TimeUnit.HOURS)
+                                 .initialCapacity(200)
+                                 .maximumSize(1000)
+                                 .build(cacheLoader);
+    }
 
     @Override
     public BlockchainContractInfo queryCacheByAddress(String contractAddr) {
-        BlockchainContractInfo info = this.cache.getIfPresent(contractAddr);
-        if(Objects.isNull(info)) {
-            var query = Wrappers.lambdaQuery(BlockchainContract.class).eq(BlockchainContract::getAddr,contractAddr);
-            BlockchainContract bc = this.getOne(query);
-            if(Objects.nonNull(bc)) {
-                info = new BlockchainContractInfo();
-                BeanUtils.copyProperties(bc,info);
-                if(StringUtils.isNoneBlank(bc.getAbi())) {
-                    info.setAbiDecoder(new AbiDecoder(bc.getAbi()));
-                }
-                this.cache.put(contractAddr,info);
-            }
+        try {
+            return this.cache.get(contractAddr);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
         }
-        return info;
     }
 
     @Override
     public void removeCache(String contractAddr) {
         this.cache.invalidate(contractAddr);
+    }
+
+    private AbiDecoder createAbiDecoder(String abi) {
+        try {
+            return new AbiDecoder(abi);
+        } catch(Exception e) {
+            log.error("Create Abi Decoder Fail!",e);
+        }
+        return null;
     }
 }
