10975e88b5446a393419e60f92abe15dea9f7fae
chiangfire <chiangfire@localhost.localdomain>
2022-09-24 20:22:37 +0800

    fix

--- foundation-api/src/main/java/net/foundation/mapi/controller/TestController.java
+++ foundation-api/src/main/java/net/foundation/mapi/controller/TestController.java
@@ -2,8 +2,12 @@ package net.foundation.mapi.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.foundation.mbusiness.domain.BlockchainContractInfo;
+import net.foundation.mbusiness.service.BlockchainContractService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "测试Api")
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private BlockchainContractService blockchainContractService;
+
     @GetMapping("/get")
     @ApiOperation(value = "测试Get请求")
-    public String get(){
-
+    public String get(@RequestParam("param") String param) {
+        BlockchainContractInfo blockchainContractInfo = blockchainContractService.queryCacheByAddress(param);
+        System.err.println(blockchainContractInfo);
         return "测试数据";
     }
 
